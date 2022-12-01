4fe95c271d85d157972e1aeabfa18f7d58d5606f
Mike Bell <mikel3377@gmail.com>
2022-12-01 08:42:05 -0500

    Day 01

--- 01/solution.js
+++ 01/solution.js
@@ -1,7 +1,9 @@
-const parseInput = input => input;
+const sum = require('../utils/sum');
 
-const runPart1 = input => input;
+const parseInput = input => input.split('\r\n\r\n').map(r => r.split('\r\n').map(n => +n));
 
-const runPart2 = input => input;
+const runPart1 = input => Math.max(...input.map(r => sum(r)));
+
+const runPart2 = input => sum(input.map(r => sum(r)).sort((a, b) => b - a).slice(0, 3));
 
 module.exports = {parseInput, runPart1, runPart2};
